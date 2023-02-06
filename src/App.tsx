85ffbfba08dd73a2295edf2dbe66932209720299
Allan Furlani Nishigouri <allan.nishigouri@koin.com.br>
2023-02-06 13:24:26 -0300

    Config react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
