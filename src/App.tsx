67be0e210126b0ac6a265cc0793eb6f05c2ebf67
Allan Furlani Nishigouri <allan.nishigouri@koin.com.br>
2023-02-06 13:18:02 -0300

    config theme and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
