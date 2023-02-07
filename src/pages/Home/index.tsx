0e6b7d79565a5d0c8b87e4ab8e6d5e66e3b249ed
Allan Furlani Nishigouri <allan.nishigouri@koin.com.br>
2023-02-07 08:02:33 -0300

    Adicionada validação nos campos do formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,9 +9,21 @@ import {
   Separator,
   TaskInput,
 } from './styles'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ter no mínimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+})
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
 
   const task = watch('task')
   const minutesAmount = watch('minutesAmount')
